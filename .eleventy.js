2bf5418fe140dc1a8415dad6bd22cc7cfb517630
HeadGofer <michaelopton@gmail.com>
2025-05-05 14:34:43 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
+
+  eleventyConfig.addCollection("post", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md");
+  });
 
   return {
     dir: {
